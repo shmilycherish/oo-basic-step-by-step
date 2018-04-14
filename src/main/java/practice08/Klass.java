8f6b8e5c95419db03ae2b346d46973028f6a768c
Cherish <shmilycherish@gmail.com>
2018-04-14 18:05:33 +0800

    implement practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,41 @@
 package practice08;
 
+import static java.lang.String.format;
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return format("Class %s", this.number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof practice07.Klass)) {
+            return false;
+        }
+
+        Klass test = (Klass) obj;
+        return this.number == test.number;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,40 @@
 package practice08;
 
+import static java.lang.String.format;
+
 public class Person {
+    private int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return format("My name is %s. I am %s years old.", this.name, this.age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !(obj instanceof Person)) {
+            return false;
+        }
+
+        Person test = (Person) obj;
+        return this.id == test.id;
+    }
 }
