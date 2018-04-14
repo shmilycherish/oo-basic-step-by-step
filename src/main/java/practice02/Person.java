da37755a45f4d93762363fc827133894873b7a4b
Cherish <shmilycherish@gmail.com>
2018-04-14 15:41:29 +0800

    implement practice2

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,26 @@
 package practice02;
 
+import static java.lang.String.format;
+
 public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return format("My name is %s. I am %s years old.", this.name, this.age);
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,32 @@
 package practice02;
 
+import static java.lang.String.format;
+
 public class Student {
+    private final String name;
+    private final int age;
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return format("I am a Student. I am at Class %s.", this.klass);
+
+    }
 }
