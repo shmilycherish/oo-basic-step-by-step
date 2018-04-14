c110a67d21f95feab94e163c09aaec12416d9efc
Cherish <shmilycherish@gmail.com>
2018-04-14 15:45:17 +0800

    implement practice3

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,24 @@
 package practice03;
 
 public class Worker {
+    private final String name;
+    private final int age;
+
+    public Worker(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
